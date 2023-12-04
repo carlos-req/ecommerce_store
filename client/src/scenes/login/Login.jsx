4f678f56577719067a8a40f1b3a39e86de13d0bf
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2023-12-03 22:16:01 -0500

    added new form

--- client/src/scenes/login/Login.jsx
+++ client/src/scenes/login/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
         <h1 className="text-xl font-black tracking-tighter uppercase text-slate-100">
           log into my account
         </h1>
-        <div className="flex flex-col w-full form-item">
+        <div className="flex flex-col w-full px-16 form-item">
           <label className="text-xs font-black text-slate-200">
             Email Address:
           </label>
@@ -34,7 +34,7 @@ const Login = () => {
             onChange={handleChange}
           ></input>
         </div>
-        <div className="flex flex-col w-full form-item">
+        <div className="flex flex-col w-full px-16 form-item">
           <label className="text-xs font-black text-slate-200">Password:</label>
           <input
             className="w-full px-6 py-4 my-4 text-xs bg-transparent border-2 border-solid outline-none border-slate-100 rounded-2xl caret-slate-100"
@@ -44,15 +44,15 @@ const Login = () => {
             onChange={handleChange}
           ></input>
         </div>
-        <div className="w-full form-item">
+        <div className="w-full px-16 form-item">
           <button className="px-10 py-4 uppercase bg-slate-100 bg-opacity-80 text-[#0d0d0d] font-black  tracking-tighter text-md w-full rounded-2xl transation ease-in-out duration-200">
             Log In
           </button>
         </div>
         <div className="form-item">
           <h3 className="text-xs text-slate-200">
-            New to Mentality?
-            <Link to="/register" className="text-white">
+            New to Mentality?&nbsp;
+            <Link to="/register" className="font-bold text-white">
               Create An Account
             </Link>
           </h3>
--- client/src/scenes/register/Register.jsx
+++ client/src/scenes/register/Register.jsx
@@ -17,48 +17,7 @@ const Register = () => {
     });
   };
   return (
-    <section className="absolute left-0 w-full top-1/4">
-      <form className="flex flex-col items-center gap-6 mx-auto w-[80vw] max-w-[30rem]">
-        <h1 className="text-xl font-black tracking-tighter uppercase text-slate-100">
-          log into my account
-        </h1>
-        <div className="flex flex-col w-full form-item">
-          <label className="text-xs font-black text-slate-200">
-            Email Address:
-          </label>
-          <input
-            className="w-full px-6 py-4 my-4 text-xs bg-transparent border-2 border-solid outline-none border-slate-100 rounded-2xl caret-slate-100"
-            type="email"
-            placeholder="Email"
-            name="email"
-            onChange={handleChange}
-          ></input>
-        </div>
-        <div className="flex flex-col w-full form-item">
-          <label className="text-xs font-black text-slate-200">Password:</label>
-          <input
-            className="w-full px-6 py-4 my-4 text-xs bg-transparent border-2 border-solid outline-none border-slate-100 rounded-2xl caret-slate-100"
-            type="password"
-            placeholder="Password"
-            name="password"
-            onChange={handleChange}
-          ></input>
-        </div>
-        <div className="w-full form-item">
-          <button className="px-10 py-4 uppercase bg-slate-100 bg-opacity-80 text-[#0d0d0d] font-black  tracking-tighter text-md w-full rounded-2xl transation ease-in-out duration-200 ">
-            Create my account
-          </button>
-        </div>
-        <div className="form-item">
-          <h3 className="text-xs text-slate-200">
-            Mentality Member?
-            <Link to="/login" className="text-white">
-              Log Back in
-            </Link>
-          </h3>
-        </div>
-      </form>
-    </section>
+    
   );
 };
 
