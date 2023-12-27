1e88fafc7111b4ee5d838aa65e0c4a50f0f5348c
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2023-12-26 23:30:30 -0500

    Added basic profile page structure

--- client/src/scenes/profile/Profile.jsx
+++ client/src/scenes/profile/Profile.jsx
@@ -1,5 +1,25 @@
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  let fName = "johhny";
+  let date = "2023-12-10";
+  return (
+    <main className="w-full h-screen mt-24 ">
+      <section>
+        <h1 className="text-4xl font-bold text-slate-100">
+          Welcome back, {fName}! 👋
+        </h1>
+        <p className="font-bold text-slate-400">customer since {date}</p>
+
+        <div>
+          <button className="uppercase">logout</button>
+        </div>
+      </section>
+
+      <section className="flex flex-wrap items-center justify-center gap-8">
+        <h3 className="text-xl uppercase">My orders</h3>
+        {/* Table of ourders */}
+      </section>
+    </main>
+  );
 };
 
 export default ProfilePage;
