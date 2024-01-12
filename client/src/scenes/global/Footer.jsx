51a6c799b07fc8c24117e9120b3dad6e1f2d104b
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2024-01-12 10:56:24 -0500

    update footer responsiveness

--- client/src/scenes/global/Footer.jsx
+++ client/src/scenes/global/Footer.jsx
@@ -1,5 +1,12 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import {
+  FaTwitter,
+  FaInstagram,
+  FaTiktok,
+  FaFacebook,
+  FaYoutube,
+} from "react-icons/fa";
 
 export const FooterLinks = ({ name, path }) => {
   return (
@@ -11,23 +18,39 @@ export const FooterLinks = ({ name, path }) => {
 
 const Footer = () => {
   return (
-    <footer className="mt-auto mb-12">
-      <section>
-        <section>
-          <h6 className="px-8 text-xl font-black tracking-tight text-center uppercase text-slate-100">
+    <footer className="mt-auto mb-12 lg:px-8">
+      <section className="flex flex-col lg:flex-row-reverse lg:justify-between">
+        {/*news letter/ social media*/}
+        <section className="flex flex-col gap-6 px-8 mb-8 lg:gap-2">
+          <h6 className="px-8 text-xl font-black tracking-tight text-center uppercase text-slate-100 ">
             Sign up for Mentality newsletter
           </h6>
-          <section className="px-8">
-            {/*This would be a form that handles submission for newsletter*/}
+          {/*This would be a form that handles submission for newsletter*/}
+          <section className="flex flex-col items-center gap-6 lg:flex-row lg:gap-3">
             <input
-              className="w-full px-6 py-3 my-4 text-xs duration-200 ease-in-out bg-transparent border-2 border-solid outline-none border-[#ffffff6b] rounded-2xl caret-slate-100 text-slate-100 focus:border-slate-50"
+              className="w-full px-6 py-3  text-xs duration-200 ease-in-out bg-transparent border-2 border-solid outline-none border-[#ffffff6b] rounded-2xl caret-slate-100 text-slate-100 focus:border-slate-50  "
               type="email"
               required
               placeholder="Your Email Address"
               name="email"
             />
+            <div className="w-full lg:w-36">
+              <button
+                type="submit"
+                className="px-12 lg:px-4 py-4 lg:py-2 lg:rounded-lg  uppercase bg-slate-100 bg-opacity-80 text-[#0d0d0d] font-black  tracking-tighter text-xl lg:text-base w-full rounded-2xl transation ease-in-out duration-200 hover:bg-opacity-100 hover:drop-shadow-md "
+              >
+                Sign Up
+              </button>
+            </div>
           </section>
-          {/*news letter/ social media*/}
+          <hr className="lg:hidden"></hr>
+          <section className="flex flex-row items-center justify-between cursor-pointer lg:px-2 lg:mt-8 ">
+            <FaInstagram className="footer--icon-hover" size={20} />
+            <FaTiktok className="footer--icon-hover" size={20} />
+            <FaTwitter className="footer--icon-hover" size={20} />
+            <FaFacebook className="footer--icon-hover" size={20} />
+            <FaYoutube className="footer--icon-hover" size={20} />
+          </section>
         </section>
         <section className="flex flex-col gap-8 text-left text-slate-100 md:flex-row ">
           <section className="w-full px-4 py-4 border-2 border-[#ffffff6b] md:border-0 rounded-3xl">
