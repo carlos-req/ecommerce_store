e13dfbc940bb81003d3bd460f569012d41b9e909
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2024-01-21 16:33:44 -0500

    add product page on thumbnail click

--- client/src/scenes/product/Product.jsx
+++ client/src/scenes/product/Product.jsx
@@ -1,16 +1,43 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { FaAngleLeft } from "react-icons/fa";
 
 const Product = () => {
   const { id } = useParams();
   const { products } = useSelector((state) => state.products);
 
+  const navigate = useNavigate();
+
   const selectedProduct = products.find((product) => product._id === id);
 
   return (
-    <div className="w-full h-screen">
+    <div className="w-full h-screen mt-20 mb-24">
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        <FaAngleLeft color="#FFF" />
+        <p className="tracking-tighter uppercase text-md text-slate-100">
+          All products
+        </p>
+      </button>
       <p className="text-4xl font-bold text-zinc-200">
         {selectedProduct.productName}
+      </p>
+      <div className=" w-96">
+        <img
+          alt={`${selectedProduct.productName} product image`}
+          src={selectedProduct.imageURL}
+        />
+      </div>
+      <p className="text-xl text-slate-100">{selectedProduct.price}</p>
+      <p className="text-xl text-slate-100">{selectedProduct.description}</p>
+      <h3 className="text-lg font-black tracking-tighter uppercase text-slate-100">
+        Collection:
+      </h3>
+      <p className="text-sm uppercase text-slate-100">
+        {selectedProduct.clothingColl}
       </p>
     </div>
   );
